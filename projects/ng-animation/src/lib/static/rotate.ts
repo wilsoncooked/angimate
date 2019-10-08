5f18513018f6059360013aaac91c2c4f4556a1d5
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-10-08 09:07:45 +0200

    refactored rotate

--- projects/ng-animation/src/lib/static/rotate.ts
+++ projects/ng-animation/src/lib/static/rotate.ts
@@ -5,59 +5,30 @@ export const rotateIn = animIn;
 export const rotateOut = animOut;
 
 
-export function rotateEnterParams ( timing,  opacity, rotateAngle, scaleX, scaleY ) {
-  return transition(':enter', useAnimation(rotateIn, { params: { opacity, rotateAngle } }))
-}
-
-export function rotateExitParams (timing,  opacity, rotateAngle, scaleX, scaleY ) {
-  return  transition(':leave', useAnimation(rotateOut, { params: { opacity, rotateAngle } }))
-}
-
-export function rotateParams (rotateName, timing,  opacity, rotateAngle, scaleX, scaleY ) {
+export function rotateParams (rotateName, opacity, angleEnter, angleLeave ) {
   return trigger(rotateName, [
-    transition(':enter', useAnimation(rotateIn, { params: { opacity, timing, rotateAngle, scaleX, scaleY } })),
-    transition(':leave', useAnimation(rotateOut, { params: { opacity, timing, rotateAngle, scaleX, scaleY } }))
+    transition(':enter', useAnimation(rotateIn, { params: { opacity, rotateAngle: `${angleEnter}` } })),
+    transition(':leave', useAnimation(rotateOut, { params: { opacity, rotateAngle: `${angleLeave}` } }))
   ]);
 }
 
+export function rotateExtendedParams (rotateName, timing, opacity, angleEnter, angleLeave, scaleX, scaleY ) {
+    return trigger(rotateName, [
+    transition(':enter', useAnimation(rotateIn, { params: { opacity, timing, rotateAngle: `${angleEnter}`, scaleX, scaleY } })),
+    transition(':leave', useAnimation(rotateOut, { params: { opacity, timing, rotateAngle: `${angleLeave}`, scaleX, scaleY } }))
+  ]);
+}
 
 // ROTATE //
 
-export const rotateLeft = trigger('rotateLeft', [
-  rotateEnterParams('300ms', 0, 179.9, 1, 1),
- rotateExitParams('300ms', 0, -179.9, 1, 1)
-]);
-
-export const rotateRight = trigger('rotateRight', [
-  rotateEnterParams('300ms', 0, -179.9, 1, 1),
- rotateExitParams('300ms', 0, 179.9, 1, 1),
-]);
-
-export const rotateLeftRight = rotateParams('rotateLeftRight', '300ms', 0, 179.9, 1, 1);
-
-export const rotateRightLeft = rotateParams('rotateRightLeft', '300ms', 0, -179.9, 1, 1);
-
-export const rotateLarge = trigger('rotateLarge', [
-  rotateEnterParams('300ms', 0, -359.9, 1, 1),
-  rotateExitParams('300ms', 0, 359.9, 1, 1),
-]);
-
-export const rotateLargeSlow = trigger('rotateLargeSlow', [
-  rotateEnterParams('600ms', 0, 359.9, 1, 1),
-  rotateExitParams('600ms', 0, -359.9, 1, 1),
-]);
-
-export const rotateLeftZoom = trigger('rotateLeftZoom', [
-  rotateEnterParams('400ms', 0, -179.9, 0, 0),
- rotateExitParams('400ms', 0, 179.9, 0, 0),
-]);
-
-export const rotateRightZoom = trigger('rotateRightZoom', [
-  rotateEnterParams('400ms', 0, 179.9, 0, 0),
- rotateExitParams('400ms', 0, -179.9, 0, 0),
-]);
-export const rotateLeftRightZoom = rotateParams('rotateLeftRightZoom', '400ms', 0, 179.9, 0, 0);
-
-export const rotateRightLeftZoom = rotateParams('rotateRightLeftZoom', '400ms', 0, -179.9, 0, 0);
-
+export const rotateLeft = rotateParams('rotateLeft', 0, 179.9, -179.9);
+export const rotateRight = rotateParams('rotateRight', 0, -179.9, 179.9);
+export const rotateLeftRight = rotateParams('rotateLeftRight', 0, 179.9, 179.9);
+export const rotateRightLeft = rotateParams('rotateRightLeft', 0, -179.9, -179.9);
+export const rotateLarge = rotateParams('rotateLarge', 0,  359.9, - 359.9);
+export const rotateLargeSlow = rotateExtendedParams('rotateLargeSlow', '600ms', 1,  359.9, -359.9, 1, 1);
+export const rotateLeftZoom = rotateExtendedParams('rotateLeftZoom', '600ms', 0,  179.9, -179.9, 0, 0);
+export const rotateRightZoom = rotateExtendedParams('rotateRightZoom', '600ms', 0,  -179.9, 179.9, 0, 0);
+export const rotateLeftRightZoom = rotateExtendedParams('rotateLeftRightZoom', '600ms', 0,  179.9, 179.9, 0, 0);
+export const rotateRightLeftZoom = rotateExtendedParams('rotateRightLeftZoom', '600ms', 0,  -179.9, -179.9, 0, 0);
 
